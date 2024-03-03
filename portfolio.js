b1d8cb9168252e3a2391097f12f9b648a32e0487
Debangi <ghatak.debangi@gmail.com>
2024-03-03 20:07:16 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#type',{
+      strings : ['Web Designer','YouTuber','Blogger'],
+      typeSpeed : 70,
+      backSpeed : 70,
+      backDelay : 1000,
+      loop : true
+});
